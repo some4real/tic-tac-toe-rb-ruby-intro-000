6b2e0beb4d032557f768c537750df5b5bbcccbda
Oluwaseyi Olufemi <oluwaseyi.olufemi.e@gmail.com>
2021-05-22 23:52:28 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -69,75 +69,57 @@ end
 
 def won?(board)
   WIN_COMBINATIONS.each {|win_combo|
-    # Helper Method
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
-end
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
 
-# Define your WIN_COMBINATIONS constant
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
-WIN_COMBINATIONS = [
-  [0,1,2], # Top row
-  [3,4,5], # Middle row
-  [6,7,8], # Last row
-  [0,3,6], #
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6],
-]
-#return falsey for an empty board
-def won?(board)
-  return false if board == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    position_1 = board[index_0]
+    position_2 = board[index_1]
+    position_3 = board[index_2]
 
-  WIN_COMBINATIONS.each do |win_combo|
-    if (board[win_combo[0]] == "X" && board[win_combo[1]] == "X" && board[win_combo[2]] == "X" ) || (board[win_combo[0]] == "O" && board[win_combo[1]] == "O" && board[win_combo[2]] == "O" )
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combo
     end
-  end
-
-  false
-
-
+  }
+  return false
 end
 
 def full?(board)
-  counter = 0
-  board.each do |i|
-    if i == "X" || i == "O"
-      counter += 1
-    end
-  end
-
-  (counter == 9)? true : false
-  # if counter == 9
-  #   return true
-  # else
-  #   false
-  # end
-
+  board.all? {|index| index == "X" || index == "O"}
 end
 
 def draw?(board)
-  if full?(board) && !won?(board)
-    true
+  if !won?(board) && full?(board)
+    return true
   else
-    false
+    return false
   end
 end
 
 def over?(board)
-  if draw?(board) || won?(board)
-    true
+  if won?(board) || draw?(board)
+    return true
+  else
+    return false
   end
 end
 
-def winner(board)
-  if won?(board)
-    winner_board = won?(board)
-    return board[winner_board[0]]
+def winner (board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
   end
 end
+
 def play(board)
   until over?(board) == true
     turn(board)
