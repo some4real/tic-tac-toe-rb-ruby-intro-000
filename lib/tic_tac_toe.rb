c53a904ace085e9dacbac01bad1e48ff778cf496
Oluwaseyi Olufemi <oluwaseyi.olufemi.e@gmail.com>
2021-05-23 00:12:37 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,4 +1,4 @@
-IN_COMBINATIONS = [
+WIN_COMBINATIONS = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
@@ -72,6 +72,7 @@ def won?(board)
     index_0 = win_combo[0]
     index_1 = win_combo[1]
     index_2 = win_combo[2]
+    
 
     position_1 = board[index_0]
     position_2 = board[index_1]
